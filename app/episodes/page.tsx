618848869e48eef418eb2f6ff34fd52d2a609ffa
JKSANJAY27 <j.k.sanjay2006@gmail.com>
2025-01-24 23:00:20 +0530

    Build issue solved

--- app/episodes/page.tsx
+++ app/episodes/page.tsx
@@ -6,6 +6,7 @@ import type { Episode } from "@/types/episode";
 import { motion, AnimatePresence } from "framer-motion";
 import { useOutsideClick } from "@/hooks/use-outside-click";
 import Image from "next/image";
+import { CloseIcon } from "@/components/magicui/CloseIcon";
 
 interface Props {
     params: {
@@ -216,37 +217,3 @@ interface Props {
 }
 
 export default EpisodesPage;
-
-
-export const CloseIcon = () => {
-  return (
-    <motion.svg
-      initial={{
-        opacity: 0,
-      }}
-      animate={{
-        opacity: 1,
-      }}
-      exit={{
-        opacity: 0,
-        transition: {
-          duration: 0.05,
-        },
-      }}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      className="h-4 w-4 text-black"
-    >
-      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-      <path d="M18 6l-12 12" />
-      <path d="M6 6l12 12" />
-    </motion.svg>
-  );
-};
